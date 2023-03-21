667341bd99c81dcce37c520bc5b9c19f6b18511f
Adam Rackis <adamrackis@hotmail.com>
2023-03-21 00:09:04 -0500

    Storybook work

--- stories/1-dropzone.stories.js
+++ stories/1-dropzone.stories.js
@@ -1,52 +1,44 @@
 //import { action } from "@storybook/addon-actions";
 
 import BasicDropZoneView from "./views/BasicDropzoneView.svelte";
-// import DisabledDropzoneView from "./views/DisabledDropzoneView.svelte";
-// import CustomSlotDropzoneView from "./views/CustomSlotDropzoneView.svelte";
-// import NoClickDropZoneView from "./views/NoClickDropzoneView.svelte";
-// import NoDragDropzoneView from "./views/NoDragDropzoneView.svelte";
+import DisabledDropzoneView from "./views/DisabledDropzoneView.svelte";
+import CustomSlotDropzoneView from "./views/CustomSlotDropzoneView.svelte";
+import NoClickDropZoneView from "./views/NoClickDropzoneView.svelte";
+import NoDragDropzoneView from "./views/NoDragDropzoneView.svelte";
 
 //import intro from "./../README.md";
 
 export default {
   title: "Examples",
   component: BasicDropZoneView,
-  argTypes: {
-    "on:drop": {
-      description: "File drop handler",
-    },
-  },
 };
 
-export const X = {
-  args: {
-    "on:drop": `handleFilesSelect`,
-  },
+export const BasicDropzone = {
+  render: () => ({
+    Component: BasicDropZoneView,
+  }),
 };
 
-/*
-export const BasicDropzone = () => ({
-  Component: BasicDropZoneView,
-  props: {},
-});
+export const DisabledDropzone = {
+  render: () => ({
+    Component: DisabledDropzoneView,
+  }),
+};
 
-export const DisabledDropzone = () => ({
-  Component: DisabledDropzoneView,
-  props: {},
-});
+export const CustomSlotDropzone = {
+  render: () => ({
+    Component: CustomSlotDropzoneView,
+  }),
+};
 
-export const CustomSlotDropzone = () => ({
-  Component: CustomSlotDropzoneView,
-  props: {},
-});
+export const DropZoneWithClickDisabled = {
+  render: () => ({
+    Component: NoClickDropZoneView,
+  }),
+};
 
-export const DropZoneWithClickDisabled = () => ({
-  Component: NoClickDropZoneView,
-  props: {},
-});
-
-export const DropZoneWithDragDisabled = () => ({
-  Component: NoDragDropzoneView,
-  props: {},
-});
-*/
+export const DropZoneWithDragDisabled = {
+  render: () => ({
+    Component: NoDragDropzoneView,
+  }),
+};
