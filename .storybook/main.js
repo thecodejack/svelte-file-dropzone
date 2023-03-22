a71ce5c5b9e6e5f8c1017f3d0837034a9bbb69a8
Adam Rackis <adamrackis@hotmail.com>
2023-03-21 20:09:23 -0500

    Cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,6 @@
 export default {
   stories: ["../stories/**/*.stories.js"],
-  addons: [
-    //"@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    // "@storybook/addon-storysource",
-    // "@storybook/addon-notes/register",
-    // "@storybook/addon-mdx-gfm",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: {
     name: "@storybook/sveltekit",
     options: {},
