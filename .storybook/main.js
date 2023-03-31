3c8f03fc31742e2380773c465d8e45230bc393db
Adam Rackis <arackaf@users.noreply.github.com>
2023-03-30 20:16:47 -0500

    Arackis/storybook upgrade (#130)
    * first story rendering
    
    * Storybook work
    
    * Remove show code buttons
    
    * First demos working
    
    * Cleanup
    
    * Final cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,11 @@
-module.exports = {
+export default {
   stories: ["../stories/**/*.stories.js"],
-  addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-storysource",
-    "@storybook/addon-notes/register",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  framework: {
+    name: "@storybook/sveltekit",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
 };
