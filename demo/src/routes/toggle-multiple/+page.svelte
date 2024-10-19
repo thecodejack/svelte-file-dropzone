e21f37dcde9e9c55db0e9371b51b1f203a9e7158
Adam Rackis <arackaf@users.noreply.github.com>
2024-10-19 14:42:34 -0600

    Arackis/fixes (#168)
    * Allow for Svelte 5 in peer deps
    
    * Adjust text for multiple

---
+++ demo/src/routes/toggle-multiple/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import Dropzone from '../../../../src/lib/components/Dropzone.svelte';
+
+	let files = {
+		accepted: [] as any[],
+		rejected: [] as any[]
+	};
+
+	function handleFilesSelect(e: any) {
+		const { acceptedFiles, fileRejections } = e.detail;
+		files.accepted = [...files.accepted, ...acceptedFiles];
+		files.rejected = [...files.rejected, ...fileRejections];
+	}
+
+	let multiple = false;
+</script>
+
+<section>
+	<label>Multiple <input type="checkbox" bind:checked={multiple} /></label>
+	<Dropzone on:drop={handleFilesSelect} {multiple} />
+
+	<ol>
+		{#each files.accepted as item}
+			<li>{item.name}</li>
+		{/each}
+	</ol>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+</style>
